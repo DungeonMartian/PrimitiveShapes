431175c2146bbee4e72cbb747aeed9b04f179b36
DungeonMartian <131139115+DungeonMartian@users.noreply.github.com>
2023-12-29 04:27:17 -0500

    Added dash

--- Player/Player.gd
+++ Player/Player.gd
@@ -4,6 +4,11 @@ const SENSITIVITY = 0.01
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 const HITSTAGGER = 8.0 
+const DASH = 500
+const UPDASH = 100
+
+var direction
+var headDir
 
 @onready var head = $Head
 @onready var camera = $Head/Camera3D
@@ -16,7 +21,7 @@ func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func _unhandled_input(event):
-	if event is InputEventMouse:
+	if event is InputEventMouseMotion:
 		head.rotate_y(-event.relative.x * SENSITIVITY)
 		camera.rotate_x(-event.relative.y * SENSITIVITY)
 		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-65), deg_to_rad(75))
@@ -27,18 +32,21 @@ func Hit(dir):
 	pass
 		
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y -= gravity * delta
-
+	if Input.is_action_just_pressed("dash") :
+		velocity.x = lerp(velocity.x, direction.x * DASH, delta * 7)
+		velocity.z = lerp(velocity.z, direction.z * DASH, delta * 7)
+		if !is_on_floor():
+			velocity.y = lerp(velocity.y, headDir.y * UPDASH, delta * 7)
+		
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+
 	var input_dir = Input.get_vector("left", "right", "forward", "back")
-	var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	headDir = (camera.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if is_on_floor():
 		if direction:
 			velocity.x = direction.x * SPEED
@@ -46,7 +54,8 @@ func _physics_process(delta):
 		else:
 			velocity.x = lerp(velocity.x, direction.x * SPEED, delta * 7)
 			velocity.z = lerp(velocity.z, direction.z * SPEED, delta * 7)
-	else:
+	if !is_on_floor():
+		velocity.y -= gravity * delta
 		velocity.x = lerp(velocity.x, direction.x * SPEED, delta * 3)
 		velocity.z = lerp(velocity.z, direction.z * SPEED, delta * 3)
 	move_and_slide()
